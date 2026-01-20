f81ad106e88d866cbae4035d7aff5aae41c096a4
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-01-20 23:16:56 +0500

    Delete Day 15/Practice_2.js

--- Day 15/Practice_2.js
+++
@@ -1,5 +0,0 @@
-// Q1. What is the value for each variable for each line of code 
-
-let num = 5 ;           //num = 5
-let newNum = num++;     //num = 6 , newNum = 5
-newNum = ++num;         //num = 7 , newNum = 7
