ff65b4c53383394209cfa7a17a99a702b9ffb0ce
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-01-20 23:14:58 +0500

    Create traffic lights system in Practice_1.js
    Implement a basic traffic lights system with actions.

---
+++ Day 15/Practice_1.js
@@ -0,0 +1,14 @@
+// Q1. Create a traffic lights system that shows what to do based in traffic lights system
+
+let light = "green";
+
+if (light=="red") {
+    console.log("Stop");
+}
+if (light=="yellow") {
+    console.log("Slow Down");
+}
+if (light=="green") {
+    console.log("Go");
+}
+
