8227dc3f949de2f9b77c80630741bb5fe12b6c2a
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-01-20 21:55:46 +0500

    Add files via upload

---
+++ Day 14/Practice_1.js
@@ -0,0 +1,19 @@
+// Q1. What will be the value of age after code runs
+let age = 23;
+age = age + 2; 
+
+console.log(age);
+
+// so the age will be 25
+
+// Q2. What is the value of average after the code tuns.
+
+let urdu = 80;
+let english = 90;
+let math = 100;
+
+let average = (urdu+english+math)/3;
+
+console.log(average);
+
+// Average = 90
