f3088a840bfbc4fae01cf15352c554d8252f1759
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-01-22 17:37:16 +0500

    Create Q1.js

---
+++ Day 16/Assignment/Q1.js
@@ -0,0 +1,10 @@
+// Q1. Write a JavaScript program to get the first n elements of an array. [n can be any positive number].
+// For example: for array [7, 9, 0,-2] and n=3
+// Print, [7, 9, 0]
+
+let array = [7,9,0,-2];
+
+let n = 3;
+let ans = array.splice(0,n);
+
+console.log(ans);
