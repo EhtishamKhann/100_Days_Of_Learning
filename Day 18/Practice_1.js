2e8ab9ed2f26a90f9c68c2fcd97a30e5bd6c2c2c
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-02-02 23:23:21 +0500

    Create object literal for a Twitter post

--- Day 18/Practice_1.js
+++ Day 18/Practice_1.js
@@ -1,1 +1,17 @@
+// Q1. Create an object literal for the properties of a twitter post that includes -
+// username
+// content
+// likes
+// reposts
+// tags
 
+const post = {
+    username :"@ehtisham",
+    content : 'This is my first post',
+    likes : 1000,
+    reposts : 200,
+    tags : ['@shami','@khan']
+};
+
+console.log(post);
+console.log(post.tags[1]);
