b952cc1d4312ae0b5f8deb4d7f24548960ff6e0f
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-02-06 22:11:11 +0500

    Add files via upload

---
+++ Day 18/Practice2.js
@@ -0,0 +1,8 @@
+// Q.1 Generate random number from 1 - 10
+
+let random = Math.random() ;
+
+let roundRandom = Math.floor( random * 10) + 1;
+
+
+console.log(roundRandom);
---
+++ Day 18/Practice3.js
@@ -0,0 +1,17 @@
+// Q1. Genrate a random number between 1 - 100
+
+let random = Math.random() ;
+
+let roundRandom = Math.floor( random * 100) + 1;
+
+
+console.log(roundRandom);
+
+// Q.2 Generate a random number between 1 - 5
+
+let random2 = Math.random() ;
+
+let roundRandom2 = Math.floor( random2 * 5) + 1;
+
+
+console.log(roundRandom2);
