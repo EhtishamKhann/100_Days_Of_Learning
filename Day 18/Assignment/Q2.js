4011241a9ffdf30e3bddb74f7759eea428e05fc7
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-02-07 02:39:10 +0500

    Add files via upload

---
+++ Day 18/Assignment/Q2.js
@@ -0,0 +1,11 @@
+// Q2. Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name.
+
+let car = {
+    carName : "BMW",
+    model : "M7",
+    color : "blue"
+};
+
+console.log(car.carName);
---
+++ Day 18/Assignment/Q3.js
@@ -0,0 +1,13 @@
+// Qs3. Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States.
+
+let person = {
+    fullName : "Ehtisham Khan",
+    age : 22,
+    city : "Peshawar"
+};
+
+person.city = "New York";
+person.country = "United States";
+
