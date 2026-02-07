2df71375d61548e9fc54211e579921735a7943d0
Ehtisham Khan <148891574+EhtishamKhann@users.noreply.github.com>
2026-02-07 09:31:32 +0500

    Add function to print a poem

---
+++ Day 19/Practice1.js
@@ -0,0 +1,7 @@
+// Q1. Create a function that prints a poem.
+
+function printPoem(){
+    console.log(" Twinkle, twinkle, little star,\n how I wonder what you are.\n Up above the world so high,\n like a diamond in the sky.");
+}
+
+printPoem();
